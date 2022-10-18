4ecf17dc694d102b0ee80e24ba81429248d338ac
Achi Onyema Victor <softnetplac@gmail.com>
2022-10-18 08:38:52 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "school.h"
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sch = "School";
+
+	while (*sch)
+	{
+		_putchar(*sch);
+		sch++;
+
+	}
+	_putchar('\n');
+
+	return (0);
+}
