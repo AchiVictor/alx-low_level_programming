9ee50ea3813fbe755c4241c4f084d8d36d0a3165
Achi Onyema Victor <softnetplac@gmail.com>
2022-10-27 18:30:00 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
-/*my header*/
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,3 +12,4 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 int _putchar(char c);
+#endif
